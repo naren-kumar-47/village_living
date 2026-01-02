1fe0c856b1e786df6c34263f5805e8beac6263d8
NarendraKumar <narendra.kp@vation.com>
2026-01-02 11:48:46 +0530

    changed the content in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./modules/SharedComponents/Header/Header";
 import Home from "./modules/Home/Home";
-import About from "./modules/About/About";
-import Footer from "./modules/SharedComponents/Footer/Footer";
-import Contact from "./modules/Contact/Contact";
 
 const App = () => {
   return (
     <div className="village">
       <Router>
-        <Header />
-
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
-
-        <Footer />
       </Router>
     </div>
   );
