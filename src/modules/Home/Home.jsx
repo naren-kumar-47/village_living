1c90b6132e3b6235b9796c980f53ccc2093a65e6
NarendraKumar <narendra.kp@vation.com>
2026-01-05 09:35:43 +0530

    changed logo and text

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Home.css";
-import logo from "../../assets/images/village-logo.png";
+import logo from "../../assets/images/village-logo.jpg";
 
 const Home = () => {
   return (
     <div className="home-wrapper">
       <div className="home-content">
         <img src={logo} alt="Logo" className="home-logo float-logo" />
-        <h1 className="coming-soon">Coming Soon</h1>
+        <h1 className="coming-soon">Launching Soon</h1>
       </div>
     </div>
   );
